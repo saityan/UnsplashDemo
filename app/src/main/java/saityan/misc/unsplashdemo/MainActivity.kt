dc7700251a6bbee7c33642afd70b75e517835148
saityan <naminatosky@gmail.com>
2023-03-12 19:46:22 +0300

    added splash screen to the app

--- app/src/main/java/saityan/misc/unsplashdemo/MainActivity.kt
+++ app/src/main/java/saityan/misc/unsplashdemo/MainActivity.kt
@@ -5,17 +5,23 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import androidx.paging.ExperimentalPagingApi
+import coil.annotation.ExperimentalCoilApi
 import dagger.hilt.android.AndroidEntryPoint
 import saityan.misc.unsplashdemo.navigation.SetupNavGraph
 import saityan.misc.unsplashdemo.ui.theme.UnsplashDemoTheme
 
+@OptIn(ExperimentalCoilApi::class)
 @ExperimentalPagingApi
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         setContent {
             UnsplashDemoTheme {
                 val navController = rememberNavController()
